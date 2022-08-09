12f769d942e0f88df8b7ae6b705c048ec3c26187
htt_benji <benjamin.hitter@gmail.com>
2022-08-09 20:05:44 +0200

    Add files via upload

--- Display.java
+++ Display.java
@@ -1,8 +1,15 @@
 package com.mime.minefront;
 
 import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
+
+import com.mime.minefront.graphics.Render;
+import com.mime.minefront.graphics.Screen;
 
 public class Display extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
@@ -11,8 +18,22 @@ public class Display extends Canvas implements Runnable {
 	public static final int HEIGHT = 600;
 	public static final String TITLE = "Minefront Pre-Alpha 0.0.1";
 
+	// ***Game-running***
 	private Thread thread;
-	private boolean running = false;
+	private boolean running = false; // is the game running ?:Yes/No
+	private Render render;
+	private Screen screen;
+	private BufferedImage img;
+	private int[] pixels;
+
+	// start method:is the game running ?:Yes/No -->if yes->NOTHING(exit out of
+	// method) if no(else)-> start the game
+
+	public Display() {
+		screen = new Screen(WIDTH, HEIGHT);
+		img = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		pixels = ((DataBufferInt) img.getRaster().getDataBuffer()).getData();
+	}
 
 	private void start() {
 		if (running)
@@ -20,11 +41,53 @@ public class Display extends Canvas implements Runnable {
 
 		running = true;
 		thread = new Thread(this);
+		thread.start();
+
+		System.out.println("Working!");
+	}
+
+	private void stop() {
+		if (!running)
+			return;
+		running = false;
+		try {
+			thread.join();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(0);
+		}
 
 	}
 
 	public void run() {
+		while (running) {
 
+			tick();
+			render();
+		}
+	}
+
+	private void tick() {
+
+	}
+
+	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+
+		screen.render();
+
+		for (int i = 0; i < WIDTH * HEIGHT; i++) {
+			pixels[i] = screen.pixels[i];
+		}
+		Graphics g = bs.getDrawGraphics();
+		g.drawImage(img, 0, 0, WIDTH * 20, HEIGHT * 20, null);
+		g.dispose();
+		bs.show();
 	}
 
 	public static void main(String[] args) {
@@ -40,6 +103,8 @@ public class Display extends Canvas implements Runnable {
 		frame.setVisible(true);
 
 		System.out.println("Running...");
+
+		game.start();
 	}
 
 }
